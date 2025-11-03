5c6053ed604524f1f265b6965553a0d7c4c58c0d
vivekkk-007 <kumarvivek05093896@gmail.com>
2025-11-03 12:12:19 +0530

    2nd version of data saved

--- my_code.py
+++ my_code.py
@@ -17,6 +17,12 @@ df.loc[len(df.index)] = new_row_loc
 new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
 df.loc[len(df.index)] = new_row_loc2
 
+new_row_loc = {'Name': 'GF3', 'Age': 18, 'City': 'City3'}
+df.loc[len(df.index)] = new_row_loc
+
+new_row_loc = {'Name': 'GF4', 'Age': 16, 'City': 'City4'}
+df.loc[len(df.index)] = new_row_loc
+
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
@@ -28,3 +34,4 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 df.to_csv(file_path, index=False)
 
 print(f"CSV file saved to {file_path}")
+
